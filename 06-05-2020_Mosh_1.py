cd8f21514310dc87afaddf7b12e8f8960001175e
Aline Futamata <aline.f.lorenzon@gmail.com>
2020-05-08 21:03:22 +1200

    Day 5

--- 06-05-2020_Mosh_1.py
+++
@@ -1,3 +0,0 @@
-# Emoji Convertor
-
-Good Morning :)
---
+++ 08-05-2020_Mosh_2.py
@@ -0,0 +1,9 @@
+# Functions
+
+def greet_user():
+    print("Hello, there! ")
+    print("Welcome on board!")
+
+print("Start")
+greet_user()
+print("Finish")
